c939a5597099da8517c144e5bd1c5b2107bf0d3a
brunobergamasco <brunoberga77@gmail.com>
2022-04-18 20:11:06 -0300

    recycleView e getLista

---
+++ app/src/main/java/com/example/myapplication/RefLista.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import com.example.myapplication.model.Filme
+
+class RefLista {
+
+    companion object{
+        val listaFilmes = mutableListOf<Filme>()
+    }
+
+}
